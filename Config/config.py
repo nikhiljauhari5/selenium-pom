9997fb0b69c8a57cbeebaac8150d96e43446e94b
nikhiljauhari5 <nikhiljauhari5@gmail.com>
2021-08-12 23:42:11 +0530

    initial commit

---
+++ Config/config.py
@@ -0,0 +1,10 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "/Users/apple/Desktop/tasks/POMdemo/driver/chromedriver"
+    FIREFOX_EXECUTABLE_PATH = ""
+
+
+    BASE_URL = "https://github.com/login"
+    USER_NAME = "nikhiljauhari5@gmail.com"
+    PASSWORD = "Nikhil10@"
+
+    LOGIN_PAGE_TITLE ="Github Login"
